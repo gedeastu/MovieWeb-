a0fe6f4ff4b5a1da492c538b978413e9f8d9ba9f
gedeastu <gede.astu@gmail.com>
2023-06-19 22:26:54 +0700

    up

--- hellowatch/src/components/index/main.jsx
+++ hellowatch/src/components/index/main.jsx
@@ -5,16 +5,14 @@ function Main () {
   return (
     <>
     <div id="header" className='max-w-full'>
-      <div id="navbar" className='grid grid-cols-2 md:grid-cols-3 p-3'>
+      <div id="navbar" className='grid grid-cols-2 md:grid-cols-3 p-3 w-full bg-[#080B10]/20 backdrop-blur-lg fixed z-50'>
         <div id="logoAndMenu" className='flex gap-2 items-center'>
           <div id="menu" className=''>
           <label tabindex={0} id="dropdownDefaultButton" data-dropdown-toggle="dropdown" className="btn bg-transparent border-none swap swap-rotate hover:bg-transparent hover:border-none">
-              <input type="checkbox" className='hidden'/>
-              <svg class="swap-off fill-current" xmlns="http://www.w3.org/2000/svg" width="" height="3em" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
-              <svg class="swap-on fill-current" xmlns="http://www.w3.org/2000/svg" width="" height="3em" viewBox="0 0 512 512"><polygon points="400 145.49 366.51 112 256 222.51 145.49 112 112 145.49 222.51 256 112 366.51 145.49 400 256 289.49 366.51 400 400 366.51 289.49 256 400 145.49"/></svg>
+              <svg class="swap-off fill-white" xmlns="http://www.w3.org/2000/svg" width="" height="3em" viewBox="0 0 512 512"><path d="M64,384H448V341.33H64Zm0-106.67H448V234.67H64ZM64,128v42.67H448V128Z"/></svg>
           </label>
-          <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow w-44 dark:bg-gray-700">
-              <ul class="py-2  text-sm text-gray-700 dark:text-gray-200" aria-labelledby="dropdownDefaultButton">
+          <div id="dropdown" className="z-10 hidden bg-white divide-y divide-gray-100 rounded-lg shadow-md w-44 dark:bg-gray-700">
+              <ul class="py-2  text-sm text-gray-700 dark:text-gray-200 text-left" aria-labelledby="dropdownDefaultButton">
                 <li>
                   <a href="#" className="block px-4 py-2 hover:bg-gray-100 dark:hover:bg-gray-600 dark:hover:text-white">Dashboard</a>
                 </li>
@@ -32,13 +30,17 @@ function Main () {
           </div>
           <img src={logo} alt="" className='w-8' />
         </div>
-        <div id="utilities"></div>
+        <div id="utilities">
+          <div id="search">
+
+          </div>
+        </div>
       </div>
-      <div id="default-carousel" class="relative w-screen mx-auto" data-carousel="slide">
-          <div class="relative h-96 overflow-hidden rounded-lg md:h-96">
+      <div id="default-carousel" class="relative w-screen mx-auto pt-20" data-carousel="slide">
+          <div class="relative h-96 overflow-hidden rounded-lg md:h-[40rem]">
               <div class="hidden duration-700 ease-in-out" data-carousel-item>
-                <div id="pict1" class="bg-cover bg-center w-full h-full relative">
-                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-full w-full"></div>
+                <div id="pict1" class="bg-cover bg-center w-full h-full relative backdrop-brightness-200">
+                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-44 w-full"></div>
                   <div id="content">
                   </div>
                   <div class="absolute bg-gradient-to-t from-[#080B10] to-black/0 top-0 h-full w-full"></div>
@@ -47,7 +49,7 @@ function Main () {
               
               <div class="hidden duration-700 ease-in-out" data-carousel-item>
                 <div id="pict2" class="bg-cover bg-center w-full h-full relative">
-                    <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-full w-full"></div>
+                    <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 w-full h-44"></div>
                     <div id="content">
                     </div>
                     <div class="absolute bg-gradient-to-t from-[#080B10] to-black/0 top-0 h-full w-full"></div>
@@ -55,8 +57,8 @@ function Main () {
               </div>
               
               <div class="hidden duration-700 ease-in-out" data-carousel-item>
-              <div id="pict3" class="bg-cover bg-center w-full h-full relative">
-                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-full w-full"></div>
+              <div id="pict3" class="bg-cover bg-top w-full h-full relative">
+                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-44 w-full"></div>
                   <div id="content">
                   </div>
                   <div class="absolute bg-gradient-to-t from-[#080B10] to-black/0 top-0 h-full w-full"></div>
@@ -65,7 +67,7 @@ function Main () {
               
               <div class="hidden duration-700 ease-in-out" data-carousel-item>
               <div id="pict4" class="bg-cover bg-center w-full h-full relative">
-                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-full w-full"></div>
+                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-44 w-full"></div>
                   <div id="content">
                   </div>
                   <div class="absolute bg-gradient-to-t from-[#080B10] to-black/0 top-0 h-full w-full"></div>
@@ -73,8 +75,8 @@ function Main () {
               </div>
               
               <div class="hidden duration-700 ease-in-out" data-carousel-item>
-              <div id="pict5" class="bg-cover bg-center w-full h-full relative">
-                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-full w-full"></div>
+              <div id="pict5" class="bg-cover bg-top w-full h-full relative">
+                  <div class="absolute bg-gradient-to-b from-[#080B10] to-black/0 top-0 h-44 w-full"></div>
                   <div id="content">
                   </div>
                   <div class="absolute bg-gradient-to-t from-[#080B10] to-black/0 top-0 h-full w-full"></div>
