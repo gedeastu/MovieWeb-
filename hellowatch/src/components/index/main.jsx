fc18a74be45e324c2764238e4e7af67f5bb8880f
gedeastu <gede.astu@gmail.com>
2023-06-19 17:45:10 +0700

    up

---
+++ hellowatch/src/components/index/main.jsx
@@ -0,0 +1,19 @@
+import './main.css';
+import logo from '../'
+function Main () {
+  return (
+    <>
+    <div id="header" className=''>
+      <div id="navbar" className='grid grid-cols-2 md:grid-cols-3'>
+        <div id="logoAndMenu">
+          <div id="menu"></div>
+          <img src="" alt="" />
+        </div>
+        <div id="utilities"></div>
+      </div>
+    </div>
+    </>
+  );
+}
+
+export default Main;
