f98159f5ce18ae85bffe6b1738c5c6a916aa0f9b
gedeastu <gede.astu@gmail.com>
2023-06-19 17:45:11 +0700

    up

--- hellowatch/src/App.jsx
+++ hellowatch/src/App.jsx
@@ -1,8 +1,24 @@
 import './App.css';
-
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
 function App() {
   return (
     <>
+    <div className='container max-w-full bg-[#080B10] min-h-screen'>
+      <header>
+
+      </header>
+      <main className='grid grid-cols-2'>
+        <div id="content">
+
+        </div>
+        <aside>
+          
+        </aside>
+      </main>
+      <footer>
+
+      </footer>
+    </div>
     </>
   );
 }
